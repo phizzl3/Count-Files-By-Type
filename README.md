c73c898b24c3379cbb9a5588560c53dd5adcf143
Brandon <phizzl3@gmail.com>
2021-02-09 13:48:14 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CountFiles
- Step through a directory and get a count of each filetype inside.
+
+ Steps through a directory and outputs a count of each filetype inside.
+
+* Asks for a directory via drag-and-drop.
+* Walks through the directory and subdirectories and generates a dictionary of extensions and their counts.
+* Displays the totals for each extension to the console.
+* Outputs a file ("file_counts.txt") to the repo's directory with the extensions and counts.
---
+++ countfiles/countfiles.py
@@ -0,0 +1,72 @@
+import os
+
+import dropfile
+
+
+def get_counts(fdir, fdict) -> None:
+    """
+    Walks through a passed directory and counts file 
+    extensions, and adds the total counts to a dictionary.
+
+    Args:
+        fdir (str/pathlib.Path): Directory containing the files to count.
+        fdict (dict): Dictionary to add the file counts to.
+    """
+    print(" Counting files...", end="")
+
+    for _rt, _dirs, files in os.walk(fdir):
+        for each in files:
+            info = []
+            info = each.split('.')
+            fdict[info[-1].lower()] = fdict.setdefault(info[-1].lower(), 0) + 1
+
+    print("Done.")
+
+
+def display_totals(fdir, fdict) -> None:
+    """
+    Displays the file count totals to the console.
+
+    Args:
+        fdir (str/pathlib.Path): Directory containing the counted files.
+        fdict (dict): Dictionary of file counts.
+    """
+    print(f"\n {fdir}")
+    print(" File Counts:\n")
+    for k, v in fdict.items():
+        print(f" {k}: {v}")
+
+    input("\n ENTER to close...")
+
+
+def write_file(fdir, fdict) -> None:
+    """
+    Outputs file counts to a text file "file_counts.txt" 
+    located in the current repo folder.
+
+    Args:
+        fdir (str/pathlib.Path): Directory containing the counted files.
+        fdict (dict): Dictionary of file counts.
+    """
+    with open('file_counts.txt', 'w') as f:
+        f.write(f'{fdir}\n')
+        f.write('File Counts:\n\n')
+        for k, v in fdict.items():
+            f.write(f'{k}: {v}\n')
+
+
+def main() -> None:
+    """
+    Generates a empty dictionary to use, gets directory of files, 
+    and calls the other needed funcitons. 
+    """
+    files = {}
+    directory = dropfile.get()
+
+    get_counts(directory, files)
+    display_totals(directory, files)
+    write_file(directory, files)
+
+
+if __name__ == "__main__":
+    main()
